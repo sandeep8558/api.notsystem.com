08a52e048fa48165659b1bfb181628e87a4973c2
sandeep8558 <sandeep198558@gmail.com>
2025-09-12 13:51:06 +0530

    Places Dev

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientAuthController;
+use App\Http\Controllers\PlaceController;
 
 Route::get("user", [ClientAuthController::class, "user"])->middleware('auth:sanctum');
 
@@ -14,6 +15,12 @@ Route::post("forgot", [ClientAuthController::class, "forgot"]);
 Route::get("login", [ClientAuthController::class, "loginerr"])->name("login");
 
 Route::group(["middleware"=>"auth:sanctum"], function(){
+
+    /* Places Routes */
+    Route::get("places", [PlaceController::class, "places"]);
+    Route::post("places/add", [PlaceController::class, "add"]);
+    Route::post("places/update", [PlaceController::class, "update"]);
+    Route::post("places/delete", [PlaceController::class, "delete"]);
 
     /* All Sanctum Routes goes here */
 
